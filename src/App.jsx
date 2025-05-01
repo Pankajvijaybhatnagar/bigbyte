6dbe4f821a5c14133438115dac1ba6c592809312
Pankajvijaybhatnagar <135111336+Pankajvijaybhatnagar@users.noreply.github.com>
2025-05-01 14:38:39 +0530

    updted by pankaj

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ import Navbar from "./components/Navbar"
 import Footer from './components/Footer'
 import Home from "./pages/Home"
 import About from './components/Home/About';
-
-
-import Sectors from './pages/Sectors';
+import Sectors  from './pages/Sectors';
 import Aboutus from './pages/Aboutus';
 
 const App = () => {
